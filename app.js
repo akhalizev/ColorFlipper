bbfac89a400d4ae0c45de343e003a9fb4beb4fb6
akhalizev <ahalizev@gmail.com>
2020-12-25 23:51:59 -0600

    Done. Simple and random HEX func.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color'); //doesn't work with getElementsByClassName
+
+btn.addEventListener('click', function () {
+    console.log(document.body);
+    //get random between 0 and 3 for demo array on the 1st line
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);
+}
